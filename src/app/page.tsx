bea4e5bc940c451e650dea35c8d837b9189b2172
pPlanello <pplanellodev@gmail.com>
2024-10-24 23:10:46 +0200

    Revert burguer list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ import { toastSuccess } from "@/components/Notification/toastNotifications";
 import { Restaurant } from "@/components/Notification/toast.interface";
 
 const data: Array<WheelDataType> = [
-  { option: "Hundred Burguers" },
-  { option: "Hundred Burguers" },
-  { option: "Hundred Burguers" },
-  { option: "Hundred Burguers" },
-  { option: "Hundred Burguers" },
-  { option: "Hundred Burguers" },
-  { option: "Hundred Burguers" },
-  { option: "Hundred Burguers" },
-  { option: "Hundred Burguers" },
+  { option: "BrioChef Madrid" },
+  { option: "El Rancho de Santa Africa" },
+  { option: "Burmet Madrid" },
+  { option: "BDP Burguer" },
+  { option: "La Birrabar" },
+  { option: "Milwaukee Burguer" },
+  { option: "Daluburguer" },
+  { option: "El Torilurban" },
+  { option: "Cesar Burguers" },
   { option: "Hundred Burguers" },
 ];
 
